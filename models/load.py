697cb23e189b3c3d6b06a74f0259fcca207d7103
Waleed El-Geresy <wgeresy@gmail.com>
2019-03-03 23:48:54 +0000

    load.py: Fix get_latest_epoch path splitting

--- models/load.py
+++ models/load.py
@@ -7,7 +7,7 @@ def get_latest_epoch(dir='.'):
     directory = os.path.join(dir, '*.h5')
     files = glob.glob(directory)
     if len(files) is not 0:
-        file_numbers = [int(os.path.split(x)[-1].strip('.h5')) for x in files]
+        file_numbers = [int(os.path.split(os.path.splitext(x)[0])[-1]) for x in files]
         return np.max(file_numbers)
     else:
         return 0
