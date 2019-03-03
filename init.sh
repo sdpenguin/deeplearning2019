68ade45126ca7a4a3c9fa525e3619ad6de768681
Waleed El-Geresy <wgeresy@gmail.com>
2019-03-03 20:15:37 +0000

    init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+# Download git repo
+git clone https://github.com/MatchLab-Imperial/keras_triplet_descriptor
+# Download hpatches data
+wget -O hpatches_data.zip https://imperialcollegelondon.box.com/shared/static/ah40eq7cxpwq4a6l4f62efzdyt8rm3ha.zip
+unzip -q ./hpatches_data.zip
+rm ./hpatches_data.zip
+
+# Insall necessary python modules
+pip install keras opencv-python
+
+# Download my repo
+git clone https://github.com/sdpenguin/deeplearning2019
