69c8bf26f998e2b2b0a9316d952850489ce0b74b
Waleed El-Geresy <wgeresy@gmail.com>
2019-03-07 11:52:07 +0000

    Change Descriptor Learning Rate to 1e-3
    This is because the current 0.1 LR causes issues when running - the MAE
    saturates to 1.0 after a few epochs.

--- dl2019/main.py
+++ dl2019/main.py
@@ -102,7 +102,7 @@ def get_desc_generator(dir_hpatches, train_fnames, test_fnames, denoise_model, u
 def get_desc_mod(shape, model_type):
     ''' Returns a descriptor model with the default (ADAM) optimizer. '''
     desc_model = get_descriptor_model(shape, model_type)
-    optimizer = keras.optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
     desc_model.compile(loss='mean_absolute_error', optimizer=optimizer)
     return desc_model
 
