40f9f5010fe8a782cd48ff5d73b80f52662ce15d
Waleed El-Geresy <wgeresy@gmail.com>
2019-03-13 19:58:57 +0000

    init.sh: Add tensorflow-gpu
    Now installs tensorflow-gpu to combat the libcuda.so problem.

--- init.sh
+++ init.sh
@@ -25,7 +25,8 @@ fi
 apt update && apt install -y libsm6 libxext6 libxrender-dev
 
 # Insall necessary python modules
-pip install keras opencv-python tqdm pandas matplotlib
+pip install opencv-python tqdm pandas matplotlib
+pip install tensorflow-gpu
 
 # Install dl2019 Python package using the default python
 /usr/bin/env python3 setup.py install --force
